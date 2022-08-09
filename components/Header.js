82121064ade2c023afac6d2637290f68b92ca6ec
magnus411 <magnusgjerstad00@gmail.com>
2022-08-09 19:48:52 +0200

    start

---
+++ components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header className="bg-slate-900 h-12 flex justify-center">
+      <h1 className="text-white text-3xl font-semibold">Home</h1>
+    </header>
+  );
+}
+
+export default Header;
