5b494a6fe9d8880861e031b0e0830eb1ff4295d9
magnus411 <magnusgjerstad00@gmail.com>
2022-08-14 22:35:28 +0200

    RASK!

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,32 @@ const Home: NextPage = () => {
 
   const { data, error } = useSWR("/api/prosjekt", fetcher);
   if (error) return <div>Failed to load</div>;
-  if (!data) return <div>Loading...</div>;
+  if (!data)
+    return (
+      <div>
+        <Head>
+          <title>Portfolio</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Header />
+        <main className="bg-[#181826]  ">
+          <Body />
+          <OmMeg />
+        </main>
+
+        <footer className="flex h-24 w-full items-center justify-center border-t">
+          <a
+            className="flex items-center justify-center gap-2"
+            href="https://github.com/magnus411"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Powered by Gjerstad
+          </a>
+          s
+        </footer>
+      </div>
+    );
 
   console.log("console gang her");
   return (
