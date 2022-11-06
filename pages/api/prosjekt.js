52a30af0d4fcfeb19365d765bb49e9fa4eb1d826
magnus411 <magnusgjerstad00@gmail.com>
2022-11-06 12:52:09 +0100

    EnvKeys

--- pages/api/prosjekt.js
+++ pages/api/prosjekt.js
@@ -11,13 +11,10 @@ import {
 const dbInstance = collection(database, "prosjekt");
 
 export default async function handler(req, res) {
-  //Find the absolute path of the json directory
-
   let lis = [];
   const q = query(collection(database, "prosjekt"));
   const querySnapshot = await getDocs(q);
   querySnapshot.forEach((doc) => {
-    // doc.data() is never undefined for query doc snapshots
     lis.push(doc.data());
   });
 
