a29f8e5beb4cced81901b8e00aa3f193f4a815c0
Magnus411 <magnusgjerstad00@gmail.com>
2022-11-22 09:55:40 +0100

    Remove google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,12 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Analytics } from "@vercel/analytics/react";
-import GoogleAnalytics from "./../components/GoogleAnalytics";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
       <Analytics />
-      {process.env.NODE_ENV === "production" && <GoogleAnalytics />}
     </>
   );
 }
